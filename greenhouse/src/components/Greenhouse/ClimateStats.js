1bb21bb5dd93a7b3ea03dff5979567dfa3e3c808
ByronHan333 <zhan12@dons.usfca.edu>
2022-12-14 15:39:06 -0800

    upload context project

--- greenhouse/src/components/Greenhouse/ClimateStats.js
+++ greenhouse/src/components/Greenhouse/ClimateStats.js
@@ -1,14 +1,49 @@
 import './ClimateStats.css';
+import { useClimate } from '../../context/ClimateContext';
+import { useEffect, useState } from 'react';
 
 function ClimateStats() {
+  const {temperature, setTemperature, humidity, setHumidity} = useClimate();
+  const [desiredTemp, setDesiredTemperature ] = useState(65);
+  const [desiredHygro, setDesiredHygro ] = useState(50);
+
+  useEffect(()=>{
+    const tempTimeout = setTimeout(()=>{
+      if (temperature < desiredTemp) {
+        setTemperature(prevTemp => prevTemp + 1)
+      }
+
+    }, 1000)
+
+    const humidityTimeout = setTimeout(()=>{
+      if(humidity < desiredHygro){
+        setHumidity(prevTemp => prevTemp + 2)
+      }
+    },1000)
+
+    if (humidity >= desiredHygro) {
+      clearTimeout(humidityTimeout)
+    }
+
+
+    if (temperature >= desiredTemp) {
+      clearTimeout(tempTimeout)
+    }
+
+    return ()=>{
+      clearTimeout(tempTimeout)
+      clearTimeout(humidityTimeout)
+    }
+
+  },[temperature, humidity])
 
   return (
     <div className="climate-stats">
       <div className="temperature">
-        Temperature {"x"}°F
+        Temperature {temperature}°F
       </div>
       <div className="humidity">
-        Humidity {"y"}%
+        Humidity {humidity}%
       </div>
     </div>
   )
